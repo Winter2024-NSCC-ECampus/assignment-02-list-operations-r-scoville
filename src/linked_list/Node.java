840b77218eb301331ade7b92dd52806adecb445b
Rose Scoville <rosemsco@live.com>
2025-03-10 15:36:55 -0300

    Adds Node class

---
+++ src/linked_list/Node.java
@@ -0,0 +1,14 @@
+package linked_list;
+
+public class Node {
+
+    public int value;
+    Node next;
+
+    // Constructor
+    Node(int value) {
+        this.value = value;
+        this.next = null; // Note: this should initialize by default to null.
+    }
+
+}
